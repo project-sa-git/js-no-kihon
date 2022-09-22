d728f44441cff551f5a8987d94456165564236d5
project-sa-git <50135286+project-sa-git@users.noreply.github.com>
2022-09-22 17:29:26 +0900

    saved.

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ console.log("かか");
 document.getElementById("app").innerHTML = `
 <h1>Hello VanillaJS!</h1>
 <h1>Hello VanillaJS!</h1>
+<h2>Hello VanillaJS!</h2>
 <div>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
